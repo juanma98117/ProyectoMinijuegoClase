c343c4121601f71c5ae4db665b1182ae30cdc239
juanma98117 <juanma98117@alumnos.ilerna.com>
2026-03-10 17:38:06 +0100

    Clases implementadas en el main

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,12 +1,22 @@
 package main;
 
 import vista.Ventana;
+import vista.PanelCategorias;
+import vista.PanelJuego;
+import vista.PanelCreditos;
+import vista.PanelMenu;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
         new Ventana();
+        new PanelJuego();
+        new PanelMenu();
+        new PanelJuego();
+        new PanelCategorias();
+        new PanelCreditos();
+
 	}
 
 }
